6544c5acaafb47a89803cb6bea63918cde03a8fb
Roni Jacobi <roni@imubit.com>
2022-03-27 20:51:10 +0300

    sgd implementation

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from part1 import create_training_and_validation_sets, convert_pixel_intensity
+from part1 import create_training_and_validation_sets, convert_pixel_intensity, download_and_extract_cifar_10_dataset
 from part2 import Model
 
 
 def main():
-    download_and_extract_cifar_10_dataset()
+    # download_and_extract_cifar_10_dataset()
     training_set, validation_set, meta = create_training_and_validation_sets()
     convert_pixel_intensity(training_set)
     convert_pixel_intensity(validation_set)
-    model = Model()
-    learning_rate = 10
+    learning_rate = 0.3
     num_of_iterations = 5
     batch_size = 50
-    momentum_coefficient = 0.2
-    l2_regularization_coefficient = 0.2
-    standard_deviation = 0.9
-    model.training(training_set, learning_rate, num_of_iterations, batch_size,
+    momentum_coefficient = 0.1
+    l2_regularization_coefficient = 0
+    standard_deviation = 1
+    model = Model(training_set, learning_rate, num_of_iterations, batch_size,
                    momentum_coefficient, l2_regularization_coefficient,
                    standard_deviation)
+    theta = model.training()
+    model.inference(theta)
 
 
 if __name__ == "__main__":
--- part2.py
+++ part2.py
@@ -1,6 +1,9 @@
+import math
 from typing import Dict
 import numpy as np
 from part1 import random_indices, subset_of_data_set
+import warnings
+warnings.filterwarnings("error")
 
 training_set_size = 1000
 num_of_labels = 10
@@ -67,25 +70,46 @@ class Model():
     def sgd(self):
         # init wight_vectors
         self.wight_vectors = np.random.normal(loc=0, scale=self.standard_deviation,
-                                              size=(num_of_labels + 1) * training_set_shape)
-        for iteration_number in range(num_of_iterations):
+                                              size=num_of_labels * (training_set_shape + 1))
+        for iteration_number in range(self.num_of_iterations):
+            print(f'started {iteration_number} iteration')
             # Random batch
-            training_indices = random_indices(batch_size, training_set_size)
+            training_indices = random_indices(self.batch_size, training_set_size)
+            v_t = np.zeros(len(self.wight_vectors))
             # batch = subset_of_data_set(training_set, i_t)
             for training_index in training_indices:
                 x = self.training_set[b'data'][training_index]
-                for i in range(num_of_labels):
-                    theta_i, b_i = self.get_theta_b_i(i)
-                    self.wight_vectors = self.wight_vectors - self.learning_rate * self.calc_derivative(theta_i, b_i, x)
-            self.wight_vectors = self.wight_vectors - learning_rate * self.gradient(batch)
+                y = self.training_set[b'labels'][training_index]
+                v_t = self.momentum_coefficient * v_t - (1 - self.momentum_coefficient) * self.learning_rate * self.calc_derivative(x, y)
+                self.wight_vectors += v_t
+        return self.wight_vectors
 
     def training(self) -> None:
-        self.sgd()
+        theta = self.sgd()
+        with open('result.txt', 'w') as f:
+            as_str = ' '.join([str(v) for v in self.wight_vectors])
+            f.write(as_str)
+        return theta
 
     def inference(self, set_of_instances: np.ndarray) -> np.ndarray:
-        if not self.wight_vectors or not self.biases:
-            print("You need to train the model before you can run inference")
-        pass
+        # if not self.wight_vectors or not self.biases:
+        #     print("You need to train the model before you can run inference")
+        print('Inference')
+        successes = 0
+        for i in range(9999):
+            print(f'image {i}')
+            x = self.training_set[b'data'][i]
+            y = self.training_set[b'labels'][i]
+            max_result = None
+            index = 0
+            for j in range(num_of_labels):
+                theta, b = self.get_theta_b_i(j)
+                result = np.inner(theta, x) + b
+                max_result = result if not max_result else max(result, max_result)
+                index = j if result == max_result else index
+            if y == index:
+               successes += 1
+        print(successes)
 
     def get_theta_b_i(self, i):
         theta_i = self.wight_vectors[i:i + training_set_shape]
@@ -93,16 +117,48 @@ class Model():
         return theta_i, b_i
 
     def calc_exp(self, theta_i, b_i, x):
-        return np.exp(np.inner(theta_i, x) + b_i)
+        try:
+            return np.exp(np.inner(theta_i, x) + b_i)
+        except RuntimeWarning:
+            print(f"{theta_i=}")
+            print(f"{x=}")
+            print(f"{b_i=}")
 
     def calc_exp_sum(self, x):
         sum = 0
         for j in range(num_of_labels):
             theta_j, b_j = self.get_theta_b_i(j)
             sum += self.calc_exp(theta_j, b_j, x)
+        return sum
 
-    def calc_derivative(self, theta_i, b_i, x):
-        self.calc_exp(theta_i, b_i, x) / self.calc_exp_sum(x)
+    def calc_derivative(self, x, y):
+        vector = []
+        r = [0] * num_of_labels
+        r[y] = 1
+        l2_sum = 0
+        for i in range(num_of_labels):
+            theta_i, b_i = self.get_theta_b_i(i)
+            l2_sum += math.pow(np.inner(theta_i, theta_i), 2)
+        l2_sum = math.sqrt(l2_sum)
+
+        for i in range(num_of_labels):
+            theta_i, b_i = self.get_theta_b_i(i)
+            r_i = r[i]
+            for j in range(training_set_shape):
+                x_j = x[j]
+                try:
+                    derivative = x_j * self.calc_exp(theta_i, b_i, x) / (self.calc_exp_sum(x) - r_i) + self.l2_regularization_coefficient * np.inner(theta_i, theta_i) * l2_sum
+                except RuntimeWarning:
+                    print(f"{theta_i=}")
+                    print(f"{x=}")
+                    print(f"{b_i=}")
+                    print(f"{r_i=}")
+                    print(self.calc_exp(theta_i, b_i, x))
+                    print(self.calc_exp_sum(x))
+                vector.append(derivative)
+            derivative_b = self.calc_exp(theta_i, b_i, x) / (self.calc_exp_sum(x) - r_i)
+            vector.append(derivative_b)
+        return np.array(vector)
 
 
 # model = Model()
